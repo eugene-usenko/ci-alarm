cc31f41ada1ee81f3cc85a2e6a34a106d53dc02f
eugene-usenko <zhenya.usenko@gmail.com>
2019-09-17 23:45:49 +0300

    Update receiver.ino

--- receiver.ino
+++ receiver.ino
@@ -1,25 +1,29 @@
-#include <RH_ASK.h>
-#include <SPI.h>
-RH_ASK rftx;
+#include <ESP8266WiFi.h>
 
-void setup()
-{
-    Serial.begin(9600);
-    if (!rftx.init())
-        Serial.println("init failed");
+WiFiServer server(80);
+IPAddress IP(192,168,4,15);
+IPAddress mask = (255, 255, 255, 0);
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("ci-alarm", "ci-alarm");
+  WiFi.softAPConfig(IP, IP, mask);
+  server.begin();
+
+//  Serial.println();
+//  Serial.println("Server started.");
+//  Serial.print("IP: ");     Serial.println(WiFi.softAPIP());
+//  Serial.print("MAC:");     Serial.println(WiFi.softAPmacAddress());
+
+  pinMode(D3, OUTPUT);
+  digitalWrite(D3, LOW);
 }
 
-void loop()
-{
-
-    uint8_t data[5];
-    uint8_t len = sizeof(data);
-    if (rftx.recv(data, &len))
-    {
-        Serial.print("Message: ");
-        Serial.println((char *)data);
-    }
-
-    delay(1000);
-    Serial.println("tick");
+void loop() {
+  WiFiClient client = server.available();
+  if (!client) {return;}
+  String cmd = client.readStringUntil('\n');
+  if(cmd == "on")digitalWrite(D3, HIGH);
+  if(cmd == "off")digitalWrite(D3, LOW);
 }
